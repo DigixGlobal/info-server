37503212affff249a5441b8480a4a88622ea37ad
Rohit Narurkar <rohitnarurkar@protonmail.com>
2019-01-18 11:54:30 +0800

    use fail safe version of dijix (Resolves DGDG-293)

--- dijixWrapper/proposals.js
+++ dijixWrapper/proposals.js
@@ -1,7 +1,7 @@
 const dijixUtil = require('./dijixUtil');
 
 const fetchProposalVersion = async (ipfsHash) => {
-  const dijixObject = await dijixUtil.getDijix().fetch(ipfsHash);
+  const dijixObject = await dijixUtil.getDijix().failSafeFetch(ipfsHash);
   return dijixObject;
 };
 
