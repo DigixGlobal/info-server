bd18abaa6f8753db5f51a3dfbbad9e0f0a4bfe87
Rohit Narurkar <rohitnarurkar@protonmail.com>
2019-03-07 10:41:07 +0800

    [fix] special proposals added to all and count (Resolves DGDG-417)

--- routes/proposals.js
+++ routes/proposals.js
@@ -6,6 +6,7 @@ const {
   getProposal,
   getProposals,
   getSpecialProposals,
+  getSpecialProposalsCount,
 } = require('../dbWrapper/proposals');
 
 const {
@@ -25,13 +26,15 @@ router.get('/test', async (req, res) => {
 
 router.get('/count', async (req, res) => {
   const cursor = getProposalsCursor({});
+  const specialProposalsCount = await getSpecialProposalsCount();
   const result = {
-    all: await cursor.count(),
+    all: (await cursor.count()) + specialProposalsCount,
   };
   for (let proposal = await cursor.next(); proposal != null; proposal = await cursor.next()) {
     if (!result[proposal.stage]) result[proposal.stage] = 0;
     result[proposal.stage] += 1;
   }
+  result[proposalStages.PROPOSAL] += specialProposalsCount;
   return res.json({ result });
 });
 
@@ -45,7 +48,10 @@ router.get('/:stage', async (req, res) => {
   const filter = (req.params.stage === 'all') ? {} : { stage: req.params.stage };
   const proposals = await getProposals(filter);
   let specialProposals = [];
-  if (req.params.stage === proposalStages.PROPOSAL) {
+  if (
+    req.params.stage === proposalStages.PROPOSAL
+    || req.params.stage === 'all'
+  ) {
     specialProposals = await getSpecialProposals();
   }
   return res.json({ result: specialProposals.concat(proposals) });
